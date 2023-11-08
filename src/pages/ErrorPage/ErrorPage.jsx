72f55be0e5e4968ae31996f0b8d5607b09272e4f
Rifa9oct <kohinuraktherr@gmail.com>
2023-11-08 11:22:15 +0600

    designed error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,13 @@
-
+import { Link } from "react-router-dom";
+import error from "../../assets/error.jpg"
 const ErrorPage = () => {
     return (
         <div>
-            <h1>error page</h1>
+            <img className="w-full mt-12" src={error} />
+            <h1 className="text-center font-extrabold text-4xl mt-5">We can not find that page !</h1>
+            <div className="flex justify-center mb-16">
+                <Link to="/"><button className="bg-blue-500 py-2 px-4 text-white font-semibold rounded-lg mt-5 hover:scale-105">Go Home</button></Link>
+            </div>
         </div>
     );
 };
